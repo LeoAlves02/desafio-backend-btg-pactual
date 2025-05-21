e2526553fe2fb54a4d04f507a7c7398097dfa806
Leonardo Alves <leoalvesbico@email.com>
2025-05-20 21:37:11 -0300

    Primeiro commit do desafio BTG

---
+++ src/main/java/leonardoalves/btgpactual/orderms/controller/OrderResponse.java
@@ -0,0 +1,14 @@
+package leonardoalves.btgpactual.orderms.controller;
+
+import leonardoalves.btgpactual.orderms.entity.OrderEntity;
+
+import java.math.BigDecimal;
+
+public record OrderResponse(Long orderId,
+                            Long customerId,
+                            BigDecimal total) {
+
+    public static OrderResponse fromEntity(OrderEntity entity){
+        return new OrderResponse(entity.getOrderId(), entity.getCustomerId(), entity.getTotal());
+    }
+}
